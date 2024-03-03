6bfb36c3e061682ec8a8595e69a83747409105f4
ashlesh <tashleshkini@gmail.com>
2024-03-03 17:21:31 +0530

    03-03-2024

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'accounts.dart';
 import 'cart.dart';
 import 'categories.dart';
@@ -26,37 +25,42 @@ class _homePageState extends State<homePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: widgetList[myIndex],
-      bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(
-            icon: Icon(
-              Icons.home_outlined,
-              color: Colors.black,
-            ),
-            label: "Home"),
-        BottomNavigationBarItem(
-            icon: Icon(
-              Icons.category_outlined,
-              color: Colors.black,
-            ),
-            label: "Categories"),
-        BottomNavigationBarItem(
-            icon: Icon(
-              Icons.account_circle_outlined,
-              color: Colors.black,
-            ),
-            label: "Account"),
-        BottomNavigationBarItem(
-            icon: Icon(
-              Icons.shopping_cart_outlined,
-              color: Colors.black,
-            ),
-            label: "Cart"),
+      bottomNavigationBar: BottomNavigationBar(
 
-      ],onTap: (index){
-        setState(() {
-          myIndex=index;
-        });
-      },currentIndex: myIndex,),
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home_outlined,
+                color: Colors.black,
+              ),
+              label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.category_outlined,
+                color: Colors.black,
+              ),
+              label: "Categories"),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.account_circle_outlined,
+                color: Colors.black,
+              ),
+              label: "Account"),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.shopping_cart_outlined,
+                color: Colors.black,
+              ),
+              label: "Cart"),
+        ],
+        onTap: (index) {
+          setState(() {
+            myIndex = index;
+          });
+        },
+        currentIndex: myIndex,
+        backgroundColor: Colors.purple,
+      ),
     );
   }
 }
